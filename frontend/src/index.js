ead06e0f8a7fde6658237516aac2ea60dbb2c997
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2019-08-03 19:37:38 +0300

    Restructuring and installing dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('pruner-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null
+    }
+  }
+})
+
+const link = authLink.concat(httpLink)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <App />
+    </ApolloHooksProvider>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
